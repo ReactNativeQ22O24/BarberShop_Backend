77b9f6f5ead28d24b3e6daf899ff6fb4d9ff1302
CARDONA LUIS <91144533+CarLuis07@users.noreply.github.com>
2024-06-09 20:29:32 -0600

    v1.0.0.1

--- routes/apiLogin.js
+++ routes/apiLogin.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getLogin } from "../controllers/LoginController";
+import { getLogin } from "../controllers/LoginController.js";
 
 const Login = express.Router();
 Login.get("/:Usuario/:Contrasena", getLogin);
